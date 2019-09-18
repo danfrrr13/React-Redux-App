5ec7eb92db4ce93361cfdeb1bcf0fc16640a3374
Daniel Ferrer <danfrrr13@gmail.com>
2019-09-18 13:31:07 -0400

    Small change

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <h3>
+        Lambda School
         GeoLocation Information Found Through Your Public IP Address
       </h3>
       <IpInformation />
